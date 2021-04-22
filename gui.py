a31be3e04864b4b3d6e49662aa5a807057355834
Roboramv2 <sriramkraja@gmail.com>
2021-04-22 11:40:32 +0530

    initial commit

---
+++ gui.py
@@ -0,0 +1,53 @@
+from tkinter import *
+import wikipedia
+
+def main(*, a=None):
+    if a==None:
+        l = wikipedia.search(my_entry.get())
+    else:
+        l = wikipedia.search(a)
+    
+    for i in range(len(l)):
+        listbox.insert(i+1, l[i])
+        listbox.pack()  
+    button3 = Button(rightframe, text = "extract", command=trying)
+    button3.pack(padx = 3, pady = 3)
+
+def trying():
+    i = listbox.curselection()
+    i = listbox.get(i)
+    try:
+        m = wikipedia.page(title=i, auto_suggest=False)
+        print(m.content)
+    except:
+        try:
+            main(i)
+        except:
+            m = wikipedia.page(title=i, auto_suggest=True)
+            print(m.content)
+
+
+root = Tk()
+root.geometry("200x150")
+frame = Frame(root)
+frame.pack()
+ 
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+ 
+rightframe = Frame(root)
+rightframe.pack(side=RIGHT)
+
+my_entry = Entry(leftframe, width = 20)
+my_entry.insert(0,'enter search term')
+my_entry.pack(padx = 5, pady = 5)
+
+button1 = Button(leftframe, text = "Search", command=main)
+button1.pack(padx = 3, pady = 3)
+
+listbox = Listbox(rightframe)
+
+
+root.title("Test")
+root.mainloop()
+
